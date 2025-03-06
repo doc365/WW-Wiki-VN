d08ee612835ed7b69839bcd54d66706058a7cb4f
doc365 <134910297+doc365@users.noreply.github.com>
2025-03-06 20:34:39 +0700

    update package

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
 module.exports = {
     env: {
-        browser: true,
-        es2021: true,
-        node: true
+      browser: true,
+      es2021: true,
+      node: true,
     },
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
+      ecmaVersion: 12,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
-    plugins: [
-        'react'
-    ],
+    plugins: ['react'],
     rules: {
-        // Add your custom rules here
-    }
-};
+      'react/prop-types': 'off', // Disable PropTypes validation
+      'react/display-name': 'off', // Optional: Disable display name warning
+    },
+  };
+  
